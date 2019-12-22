b6c9680540958f7926dd73c7863bbdf32925d6f0
bigo <inred9@gmail.com>
2019-12-22 10:23:08 +0800

    share lib

--- aws/ec2/attach_ebs.sh
+++ aws/ec2/attach_ebs.sh
@@ -29,5 +29,8 @@ git clone https://github.com/wubigo/node-fn.git /opt/node-fn
 chown -Rf ubuntu:ubuntu /opt/node-fn
 cd /opt/node-fn/sitemap
 export PATH=/opt/node-v12.13.1-linux-x64/bin:$PATH
+
+
+sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 npm install
-node fromUrl.js > fromUrl.log
+node apify.js > apify.log
